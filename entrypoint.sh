82953812cc0ab61adcd2fea9737a521255148b28
Shawn Weeks <sweeks@weeksconsulting.us>
2020-09-28 12:53:16 -0500

    Updated to start crowd in the foreground

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,11 @@
 set -e
 umask 0027
 
-ls -lh ${CROWD_INSTALL_DIR}
-
 : ${JAVA_OPTS:=}
 : ${CATALINA_OPTS:=}
 
 export JAVA_OPTS="${JAVA_OPTS}"
 export CATALINA_OPTS="${CATALINA_OPTS}"
 
-startup() {
-    echo Starting Jira Server
-    ${CROWD_INSTALL_DIR}/start_crowd.sh
-    tail -F ${CROWD_HOME}/logs/atlassian-crowd.log
-}
-
-shutdown() {
-    echo Stopping Jira Server
-    ${CROWD_INSTALL_DIR}/stop_crowd.sh
-}
-
-trap "shutdown" INT
 entrypoint.py
-startup
+${CROWD_INSTALL_DIR}/start_crowd.sh -fg
